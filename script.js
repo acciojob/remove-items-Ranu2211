7ffb595861212244108b12acdb1bd9014137a21b
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-18 19:09:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function(){
+	const item = document.getElementById('colorSelect');
+const Button = document.querySelector('input[type="button"]');
+		
+	Button.addEventListener('click',function(){
+	const index =item.selectedIndex;
+		if(index !== -1){
+			item.remove(index);
+		}
+});
+});
+
